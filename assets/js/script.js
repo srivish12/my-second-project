e6e5db621cff1f0f8d823de3dddc6180651d16e8
srivish12 <grayan637@gmail.com>
2025-06-01 23:41:42 +0100

    second dialog box display, close functions.income  and expense buttons links with main dialog. date  and time fix.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
 const dialog= document.getElementById("dialog");
 const insertBtn =document.getElementById("insertBtn");
 const date= document.getElementById("date");
+
+insertBtn.addEventListener("click",()=> {
+    dialog.style.display="flex";
+})
+
+document.getElementById("close").addEventListener("click",()=>{
+    dialog.style.display="none";
+})
+
+function setNow(){
+    let now= new Date();
+    let year=now.getFullYear();
+    let month=(now.getMonth() + 1).toString().padStart(2, '0');
+    let day=now.getDate().toString().padStart(2, '0');
+    let hours=now.getHours().toString().padStart(2, '0');
+    let minutes=now.getMinutes().toString().padStart(2, '0');
+    date.value= `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+setNow();
+
+document.getElementById("incomeBtn").addEventListener("click",()=>{
+    saveRecord("income");
+});
+document.getElementById("expenceBtn").addEventListener("click",()=>{
+    saveRecord("expence");
+});
