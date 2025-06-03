8d7b66e5c3a0b762361e17b1bfccd36e084402c9
srivish12 <grayan637@gmail.com>
2025-06-03 11:12:24 +0100

    formatted code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,46 @@
-const dialog= document.getElementById("dialog");
-const insertBtn =document.getElementById("insertBtn");
-const date= document.getElementById("date");
-const amount=document.getElementById("amount");
+const dialog = document.getElementById("dialog");
+const insertBtn = document.getElementById("insertBtn");
+const date = document.getElementById("date");
+const amount = document.getElementById("amount");
 
-insertBtn.addEventListener("click",()=> {
-    dialog.style.display="flex";
-})
+insertBtn.addEventListener("click", () => {
+  dialog.style.display = "flex";
+});
 
-document.getElementById("close").addEventListener("click",()=>{
-    dialog.style.display="none";
-})
+document.getElementById("close").addEventListener("click", () => {
+  dialog.style.display = "none";
+});
 
-function setNow(){
-    let now= new Date();
-    let year=now.getFullYear();
-    let month=(now.getMonth() + 1).toString().padStart(2, '0');
-    let day=now.getDate().toString().padStart(2, '0');
-    let hours=now.getHours().toString().padStart(2, '0');
-    let minutes=now.getMinutes().toString().padStart(2, '0');
-    date.value= `${year}-${month}-${day}T${hours}:${minutes}`;
+function setNow() {
+  let now = new Date();
+  let year = now.getFullYear();
+  let month = (now.getMonth() + 1).toString().padStart(2, "0");
+  let day = now.getDate().toString().padStart(2, "0");
+  let hours = now.getHours().toString().padStart(2, "0");
+  let minutes = now.getMinutes().toString().padStart(2, "0");
+  date.value = `${year}-${month}-${day}T${hours}:${minutes}`;
 }
 setNow();
 
-function saveRecord(type){
-    let val;
-    if(type == "income"){
-        val=parseFloat(amount.value);
-    }else {
-    val=-parseFloat(amount.value);
-    }
-    let record={
-        date:new Date(date.value),
-        description:description.value,
-        amount:val
-    };
-    console.log(record);
-} 
+function saveRecord(type) {
+  let val;
+  if (type == "income") {
+    val = parseFloat(amount.value);
+  } else {
+    val = -parseFloat(amount.value);
+  }
+  let record = {
+    date: new Date(date.value),
+    description: description.value,
+    amount: val,
+  };
+  console.log(record);
+}
 
-document.getElementById("incomeBtn").addEventListener("click",()=>{
-    saveRecord("income");
+document.getElementById("incomeBtn").addEventListener("click", () => {
+  saveRecord("income");
 });
 
-document.getElementById("expenseBtn").addEventListener("click",()=>{
-    saveRecord("expense");
+document.getElementById("expenseBtn").addEventListener("click", () => {
+  saveRecord("expense");
 });
